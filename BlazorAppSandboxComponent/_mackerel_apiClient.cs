43446da81449db0e3766cd9e4f7e40d7aef965ad
koudenpa <koudenpa@hotmail.com>
2020-01-17 20:41:05 +0900

    コメントの整理。

--- BlazorAppSandboxComponent/_mackerel_apiClient.cs
+++ BlazorAppSandboxComponent/_mackerel_apiClient.cs
@@ -4,6 +4,7 @@ namespace MackerelApi
 {
     public partial class mackerel_apiClient
     {
+        // TODO HttpClientの公開ではなく認証情報の設定メソッドとする。
         public HttpClient HttpClient { get { return _httpClient; } }
     }
 }
